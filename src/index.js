4a263607b5a30f03b4476a707ebe595b4765205f
Toonenma <marie-anne_Toonen@reyrey.com>
2018-06-19 14:46:33 +0200

    weer een stap je verder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () =>{
-    return <div>Hi!</div>;
+import SearchBar from './components/searchBar';
+
+
+
+const AOPI_KEY = 'AIzaSyAQL9RvEEQdOIiH5wGYmpYt - Qj - ppHZoNY';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
